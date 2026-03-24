1ac8a5ca51105c33cce50c2629274ae4943591cc
loveable-curmudgeon <meganwinters742@gmail.com>
2026-03-24 00:07:52 -0600

    Add Enhanced Input: WASD movement and jump working

--- LATTICE/Source/LATTICE/LatticePlayerCharacter.cpp
+++ LATTICE/Source/LATTICE/LatticePlayerCharacter.cpp
@@ -1,6 +1,9 @@
 #include "LatticePlayerCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
 ALatticePlayerCharacter::ALatticePlayerCharacter()
 {
@@ -10,20 +13,51 @@ ALatticePlayerCharacter::ALatticePlayerCharacter()
 void ALatticePlayerCharacter::BeginPlay()
 {
     Super::BeginPlay();
-
     UGameInstance* GI = UGameplayStatics::GetGameInstance(this);
     ScaleManager = GI->GetSubsystem<ULatticeScaleManager>();
-
     if (ScaleManager)
     {
         ScaleManager->OnScaleChanged.AddDynamic(this, &ALatticePlayerCharacter::OnScaleChanged);
         ApplyScalePhysics(ScaleManager->GetCurrentScale());
+    }
+    if (APlayerController* PC = Cast<APlayerController>(GetController()))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+            ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(DefaultMappingContext, 0);
+        }
     }
 }
 
 void ALatticePlayerCharacter::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+}
+
+void ALatticePlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+    if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ALatticePlayerCharacter::Move);
+        EIC->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ALatticePlayerCharacter::Jump);
+    }
+}
+
+void ALatticePlayerCharacter::Move(const FInputActionValue& Value)
+{
+    FVector2D MovementVector = Value.Get<FVector2D>();
+    if (Controller)
+    {
+        AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+        AddMovementInput(GetActorRightVector(), MovementVector.X);
+    }
+}
+
+void ALatticePlayerCharacter::Jump()
+{
+    ACharacter::Jump();
 }
 
 void ALatticePlayerCharacter::OnScaleChanged(ELatticeScale OldScale, ELatticeScale NewScale)
@@ -34,15 +68,13 @@ void ALatticePlayerCharacter::OnScaleChanged(ELatticeScale OldScale, ELatticeSca
 void ALatticePlayerCharacter::ApplyScalePhysics(ELatticeScale Scale)
 {
     if (!ScaleManager) return;
-
     FLatticeScalePhysics Params = ScaleManager->GetPhysicsForScale(Scale);
     UCharacterMovementComponent* Movement = GetCharacterMovement();
-
     if (Movement)
     {
-        Movement->GravityScale            = Params.GravityScale;
-        Movement->MaxWalkSpeed            = Params.MovementSpeed;
-        Movement->JumpZVelocity           = Params.JumpVelocity;
-        Movement->BrakingFrictionFactor   = Params.AirResistance;
+        Movement->GravityScale          = Params.GravityScale;
+        Movement->MaxWalkSpeed          = Params.MovementSpeed;
+        Movement->JumpZVelocity         = Params.JumpVelocity;
+        Movement->BrakingFrictionFactor = Params.AirResistance;
     }
 }
--- LATTICE/Source/LATTICE/LatticePlayerCharacter.h
+++ LATTICE/Source/LATTICE/LatticePlayerCharacter.h
@@ -1,26 +1,38 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "LatticeScaleManager.h"
+#include "InputActionValue.h"
 #include "LatticePlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class LATTICE_API ALatticePlayerCharacter : public ACharacter
 {
     GENERATED_BODY()
-
 public:
     ALatticePlayerCharacter();
-
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 protected:
     UFUNCTION()
     void OnScaleChanged(ELatticeScale OldScale, ELatticeScale NewScale);
-
     void ApplyScalePhysics(ELatticeScale Scale);
+    void Move(const FInputActionValue& Value);
+    void Jump();
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    UInputMappingContext* DefaultMappingContext;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    UInputAction* MoveAction;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    UInputAction* JumpAction;
 
 private:
     UPROPERTY()
